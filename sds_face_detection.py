5875c30ecbcbb32fbcd64eceb5da43c5bce3f072
Shalin <shalinshal@users.noreply.github.com>
2018-01-13 21:51:56 +0530

    continuous face recognition
    run sds_face_detection to continuously detect and predict the face. sds_face_recognition is the program to train the model using LBPH algorithm of OpenCV. also removed the dataset of animals due to some issues.

--- sds_face_detection.py
+++ sds_face_detection.py
@@ -1,29 +1,36 @@
+# import libraries
 import cv2
-#from PIL import Image
+import sds_face_recognition
+
 cascPath ="C:/Users/Shalin/Anaconda3/Library/etc/haarcascades/"
 faceCascade = cv2.CascadeClassifier(cascPath + 'haarcascade_frontalface_default.xml' )
 eyeCascade = cv2.CascadeClassifier(cascPath + 'haarcascade_eye.xml')
 font = cv2.FONT_HERSHEY_SIMPLEX
-def detect_face(mirror=False):
+
+#face detection function
+def detect_face(face_recognizer, mirror=False):
     cam = cv2.VideoCapture(0)
+    cam.release()
     while True:
         _val, img = cam.read()
         if mirror:
-            gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #convert to gray for face detection
             face = faceCascade.detectMultiScale(gray_img,
 								 scaleFactor = 1.2,
 								 minNeighbors = 5)
-        for (x, y, w, h) in face:
+        for (x, y, w, h) in face:                   #detect faces in the image
             cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
-            cv2.putText(img,'Shalin',(x,y-5), font, .6,(0,255,0),1,cv2.LINE_AA)
+            name = sds_face_recognition.predict(face_recognizer, img)
+            cv2.putText(img, name, (x,y-5), font, .6,(0,255,0),1,cv2.LINE_AA)
         cv2.imshow('frame',img)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(5) & 0xFF == ord('q'):       #quit program on pressing 'Q'
             break
     cam.release()
     cv2.destroyAllWindows()    
 
 def main():
-    detect_face(mirror=True)
+    face_recognizer = sds_face_recognition.main()
+    detect_face(face_recognizer, mirror=True)
 
 if __name__ == '__main__':
 	main()
